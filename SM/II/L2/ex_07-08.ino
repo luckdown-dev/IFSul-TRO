adb988f2ceb76056bf72bcb0cfbe87defa242934
Álisson Becker <81471563+luckdown-dev@users.noreply.github.com>
2023-03-19 21:43:07 -0300

    Add files via upload

---
+++ SM/II/L2/ex_07-08.ino
@@ -0,0 +1,39 @@
+#define DATA 2
+#define CLOCK 4
+#define LATCH 3
+
+void setup(){
+  pinMode(DATA, OUTPUT);   //Configuração inicial dos pinos
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  
+  digitalWrite(CLOCK, 0);  //Configurando estado inicial de clock e latch
+  digitalWrite(LATCH, 0);
+  
+  desligaLeds();  //Configura o estado inicial dos leds
+  digitalWrite(DATA, 1);
+  
+}
+
+void loop(){
+  pulso(CLOCK);
+  pulso(LATCH);
+  delay(250);
+  digitalWrite(DATA, !digitalRead(DATA));
+  
+}
+
+void desligaLeds(){
+  digitalWrite(DATA, 0);
+  
+  for(int c1=1; c1<=8; c1++){
+    pulso(CLOCK);
+  }
+  
+  pulso(LATCH);
+}
+
+void pulso(int pino){
+  digitalWrite(pino, 1);
+  digitalWrite(pino, 0);
+}
---
+++ SM/II/L2/ex_12-13.ino
@@ -0,0 +1,44 @@
+#define DATA 2
+#define CLOCK 4
+#define LATCH 3
+
+void setup(){
+  pinMode(DATA, OUTPUT);   //Configuração inicial dos pinos
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  
+  digitalWrite(CLOCK, 0);  //Configurando estado inicial de clock e latch
+  digitalWrite(LATCH, 0);
+  
+  desligaLeds();  //Configura o estado inicial dos leds
+  digitalWrite(DATA, 1);
+}
+
+void loop(){
+  for(int c1=1; c1<=8; c1++){
+    if(c1==1){
+      digitalWrite(DATA, 1);
+    } else {
+      digitalWrite(DATA, 0);
+    }
+    
+    pulso(CLOCK);
+    pulso(LATCH);
+    delay(500);
+  }
+}
+
+void desligaLeds(){
+  digitalWrite(DATA, 0);
+  
+  for(int c1=1; c1<=8; c1++){
+    pulso(CLOCK);
+  }
+  
+  pulso(LATCH);
+}
+
+void pulso(int pino){
+  digitalWrite(pino, 1);
+  digitalWrite(pino, 0);
+}
