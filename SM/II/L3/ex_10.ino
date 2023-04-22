125f3ef296693d8102a5cb66fa9cf4ada6ab310b
Álisson Becker <81471563+luckdown-dev@users.noreply.github.com>
2023-04-22 18:57:59 -0300

    Create ex_10.ino

---
+++ SM/II/L3/ex_10.ino
@@ -0,0 +1,53 @@
+#define DATA 5
+#define CLOCK 7
+#define LATCH 6
+
+byte vLetra[] = {B11101110, B10111100, B11111100, B01110000};
+byte vDisplay[] = {B11101111, B11011111, B10111111, B01111111};
+
+void setup(){
+  configuraDisplay7s();
+
+  shiftOut(DATA, CLOCK, LSBFIRST, vDisplay[0]);
+  shiftOut(DATA, CLOCK, LSBFIRST, vLetra[0]);
+  enviaPulso(LATCH);
+
+  delay(750);
+
+  limpaDisplay();
+  
+}
+
+void loop(){
+
+}
+
+void limpaDisplay(){
+  for(int c=0; c<=3; c++){
+    shiftOut(DATA, CLOCK, LSBFIRST, vDisplay[c]);
+    shiftOut(DATA, CLOCK, LSBFIRST, B00000000);
+    enviaPulso(LATCH);
+  }
+}
+
+void atualizaDisplay(){
+  for(int c=0; c<=3; c++){
+    shiftOut(DATA, CLOCK, LSBFIRST, vDisplay[c]);
+    shiftOut(DATA, CLOCK, LSBFIRST, vLetra[c]);
+    enviaPulso(LATCH);
+  }
+}
+
+void configuraDisplay7s(){
+  pinMode(DATA, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+
+  digitalWrite(CLOCK, 0);
+  digitalWrite(LATCH, 0);
+}
+
+void enviaPulso(int pino){
+  digitalWrite(pino, 1);
+  digitalWrite(pino, 0);
+}
