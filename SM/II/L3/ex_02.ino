e15b8456175434a0a0eb2d474875553054530a16
Álisson Becker <81471563+luckdown-dev@users.noreply.github.com>
2023-04-22 18:30:16 -0300

    Create ex_02.ino

---
+++ SM/II/L3/ex_02.ino
@@ -0,0 +1,30 @@
+#define DATA 5
+#define CLOCK 7
+#define LATCH 6
+
+void setup(){
+  configuraDisplay7s();
+  shiftOut(DATA, CLOCK, LSBFIRST, B11011111); // display ativa com nivel baixo
+  shiftOut(DATA, CLOCK, LSBFIRST, B10111110);  // segmentos
+
+  enviaPulso(LATCH);
+
+}
+
+void loop(){
+
+}
+
+void configuraDisplay7s(){
+  pinMode(DATA, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+
+  digitalWrite(CLOCK, 0);
+  digitalWrite(LATCH, 0);
+}
+
+void enviaPulso(int pino){
+  digitalWrite(pino, 1);
+  digitalWrite(pino, 0);
+}
