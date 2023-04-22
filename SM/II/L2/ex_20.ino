fcaf148e6bab7e20beb2fcf63c5698638eef5312
Álisson Becker <81471563+luckdown-dev@users.noreply.github.com>
2023-04-22 18:17:08 -0300

    Update ex_20.ino

--- SM/II/L2/ex_20.ino
+++ SM/II/L2/ex_20.ino
@@ -6,12 +6,9 @@ void setup(){
   configuraPinos();
   desligaLeds();
 
-  for(int i=1; i<=5; i++){
+  for(int i=10; i<=20; i++){
     desligaLeds();
-    exibeBitMSB(228);
-    delay(500);
-    desligaLeds();
-    exibeBitMSB(125);
+    exibeBitLSB(i);
     delay(500);
   }
 
@@ -23,9 +20,9 @@ void loop(){
 
 // *--------* FUNÇÕES *--------* //
 
-void exibeBitMSB(int valor){
-  int estado = valor & 128;
-  shiftOut(DATA, CLOCK, LSBFIRST, estado);
+void exibeBitLSB(int valor){
+  int estado = valor & 1;
+  shiftOut(DATA, CLOCK, MSBFIRST, estado);
   enviaPulso(LATCH);
 }
 
