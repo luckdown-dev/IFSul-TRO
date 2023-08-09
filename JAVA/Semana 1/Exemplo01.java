f3431e5194738ba1c03b254bebcf890b49f83756
Álisson Becker <81471563+luckdown-dev@users.noreply.github.com>
2023-08-09 15:07:19 -0300

    Exemplo01

---
+++ JAVA/Semana 1/Exemplo01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exemplo01
+{
+	public static void main(String[] args) {
+	    int numb1, numb2, soma;
+	    
+	    Scanner teclado = new Scanner(System.in);
+	    
+		System.out.print("Informe o primeiro valor: ");
+		numb1 = teclado.nextInt();
+		
+		System.out.print("Informe o segundo valor: ");
+		numb2 = teclado.nextInt();
+		
+		soma = numb1+numb2;
+		
+		System.out.print("O resultado e: " + soma);
+	}
+}
